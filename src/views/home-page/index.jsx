8b4f32ab44bf1f8a9ea5fa82552a52062515194d
korealu <643949593@qq.com>
2022-10-29 15:49:13 +0800

    demo

--- src/views/home-page/index.jsx
+++ src/views/home-page/index.jsx
@@ -1,21 +1,9 @@
-import FDRequest from "@/services";
 import React, { memo } from "react";
-import { useEffect } from "react";
 
 
 import {HomeWrap} from "./style";
 
 const HomePage = memo(() => {
-  useEffect(() => {
-    async function getData() {
-      const data = await FDRequest.get({
-        url: "/home/highscore",
-      });
-      console.log(data);
-    }
-    getData()
-  }, []);
-
   return <HomeWrap>HomePage</HomeWrap>;
 });
 
--- src/views/home-page/style/index.jsx
+++ src/views/home-page/style/index.jsx
@@ -3,5 +3,4 @@ import styled from "styled-components";
 
 export const HomeWrap = styled.div`
   width: 100%;
-  background-color: #ccc;
 `
