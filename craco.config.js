f443a2f6f258060bff74831bf9411a48ff6f5fdc
korealu <643949593@qq.com>
2023-06-12 14:23:59 +0800

    新电脑试跑 2023-06-12 14:23:45

--- craco.config.js
+++ craco.config.js
@@ -1,41 +1,41 @@
-const path = require('path');
-const CracoLessPlugin = require('craco-less');
+const path = require("path");
+const CracoLessPlugin = require("craco-less");
 
-const resolve = pathName => path.resolve(__dirname, pathName)
+const resolve = (pathName) => path.resolve(__dirname, pathName);
 
 module.exports = {
   // less 配置
-  plugins: [{
-    plugin: CracoLessPlugin,
-    options: {
-      lessLoaderOptions: {
-        lessOptions: {
-          modifyVars: {
-            '@primary-color': '#1DA57A'
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              "@primary-color": "#1DA57A",
+            },
+            javascriptEnabled: true,
           },
-          javascriptEnabled: true,
         },
       },
     },
-  }, ],
+  ],
   webpack: {
     alias: {
-      '@': resolve('src'),
-      'components': resolve('src/components'),
-      'utils': resolve('src/utils')
+      "@": resolve("src"),
+      components: resolve("src/components"),
+      utils: resolve("src/utils"),
     },
     // 配置打包文件夹名称, 静态文件路径
-    configure: (webpackConfig, {
-      paths
-    }) => {
-      paths.appBuild = 'dist'
+    configure: (webpackConfig, { paths }) => {
+      paths.appBuild = "dist";
       webpackConfig.output = {
         ...webpackConfig.output,
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: process.env.NODE_ENV === 'production' ? '/react-airbnb/' : ''
-      }
+        path: path.resolve(__dirname, "dist"),
+        // publicPath: process.env.NODE_ENV === 'production' ? '/react-airbnb/' : ''
+      };
       return webpackConfig;
-    }
+    },
   },
   // 配置代理
   // devServer: {
@@ -49,6 +49,6 @@ module.exports = {
   //     }
   //   },
   // },
-}
+};
 
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
